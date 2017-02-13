23060285bb70f98d8b9f714cd66c9d34a59a0520
bdebenon <bdebenon@tamu.edu>
2017-02-12 18:06:31 -0600

    Update 3

--- Team Project One/Team Project One/DatabaseLibrary.h
+++ Team Project One/Team Project One/DatabaseLibrary.h
@@ -16,7 +16,7 @@ namespace DatabaseLibrary
 	private:
 		static DATABASELIBRARY_API std::set<Table*> tables;
 	public:
-		static DATABASELIBRARY_API Database();
+		DATABASELIBRARY_API Database();
 		static DATABASELIBRARY_API void addTable(Table* t, std::string name);
 		static DATABASELIBRARY_API void dropTable(std::string name);
 		static DATABASELIBRARY_API std::set<std::string> printTableNames();
@@ -29,23 +29,22 @@ namespace DatabaseLibrary
 		static DATABASELIBRARY_API std::set<std::string> attributes;
 		static DATABASELIBRARY_API std::set<Record*> records;
 	public:
-		static DATABASELIBRARY_API Table();
-		static DATABASELIBRARY_API Table(std::list<std::string> names); //attribute names
-		static DATABASELIBRARY_API void addAttrib(std::string name);
-		//adds column to END of table with that new attrib
-		//entries currently in table get NULL for this attrib
+		DATABASELIBRARY_API Table();
+		DATABASELIBRARY_API Table(std::list<std::string> names); //attribute names
+		static DATABASELIBRARY_API void addAttrib(std::string name);	//adds column to END of table with that new attrib
+																		//entries currently in table get NULL for this attrib
 		static DATABASELIBRARY_API void deleteAttrib(std::string name);
 		static DATABASELIBRARY_API void insertRec(Record* r);
-		static DATABASELIBRARY_API set<string> getAttrib();
+		static DATABASELIBRARY_API std::set<std::string> getAttrib();
 		static DATABASELIBRARY_API int getSize(); //returns number of records
 											 //An iterator of some sort that can be used to return individual records from the 
 											 //table. There are many ways this can be done.
-		static DATABASELIBRARY_API void setKey(string attribName);
+		static DATABASELIBRARY_API void setKey(std::string attribName);
 		static DATABASELIBRARY_API Table* crossJoin(Table* t1, Table* t2);
 		static DATABASELIBRARY_API Table* naturalJoin(Table* t1, Table* t2);
-		static DATABASELIBRARY_API int count(string attribName);
-		static DATABASELIBRARY_API std::string getMax(string attribName); //idk if int
-		static DATABASELIBRARY_API std::string getMin(string attribName); //idk if int
+		static DATABASELIBRARY_API int count(std::string attribName);
+		static DATABASELIBRARY_API std::string getMax(std::string attribName); //idk if int
+		static DATABASELIBRARY_API std::string getMin(std::string attribName); //idk if int
 	};
 
 	class Record {
@@ -53,7 +52,7 @@ namespace DatabaseLibrary
 		static DATABASELIBRARY_API int size;
 		static DATABASELIBRARY_API std::list<std::string> entries;
 	public:
-		static DATABASELIBRARY_API Record(int s) : size(s);
+		DATABASELIBRARY_API Record(int s);
 		static DATABASELIBRARY_API int getSize();
 		//Accessor function, like r[i]
 	};
